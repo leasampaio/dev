3865bda1ee05aa2892bef73e75d8bb745a8f854b
Lea Sampaio <lealisboas@gmail.com>
2020-08-26 16:47:55 -0300

    atv aula 16

--- node/aula2.js
+++ node/aula2.js
@@ -1,2 +1,14 @@
 const fs = require("fs");
-const stream = fscreateReadStream
+const stream = fs.createReadStream ('countries.csv');
+const csvParser = require('csv-parser');
+
+let conteudo =[];
+stream.pipe(csvParser()).on('data', (data)=>{
+    conteudo.push ({
+        Country: data['Country'].trim(),
+        Region: data ['Region'].trim(),
+        Population: Number(data['Population']),
+        "Pop. Density (per sq. mi.)": Number(data['Pop. Density (per sq. mi.)'].replace(',','.'))
+    });
+    console.log("Conteúdo", conteudo[0]);
+})
